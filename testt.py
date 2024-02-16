e55176ba1fa0bdb453fc41c5f1c7141f90e59b82
M3MAI <160223496+M3MAI@users.noreply.github.com>
2024-02-16 14:41:14 +0200

    Up testt.py

--- testt.py
+++ testt.py
@@ -16,4 +16,4 @@ def index():
     return render_template('index.html', default_printer=default_printer)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True)
