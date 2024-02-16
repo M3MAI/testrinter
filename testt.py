5cffe1da1aeeb7f52aaaf00ce09d9bf3d16155bb
M3MAI <160223496+M3MAI@users.noreply.github.com>
2024-02-16 14:00:11 +0200

    add

---
+++ testt.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import win32print
+
+app = Flask(__name__)
+
+def get_default_printer():
+    try:
+        default_printer = win32print.GetDefaultPrinter()
+        return default_printer
+    except Exception as e:
+        return str(e)
+
+@app.route('/')
+def index():
+    default_printer = get_default_printer()
+    return render_template('index.html', default_printer=default_printer)
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
